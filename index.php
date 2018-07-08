52bfd18ef620013a4b788ef5f825b80647384826
廣田真由 <my-hrt@i.softbank.jp>
2018-07-08 14:30:58 +0900

    じゃねーよ

--- index.php
+++ index.php
@@ -1,50 +1,51 @@
 <?php
-// HTTPヘッダを設定
-$channelToken = 'I4DUva3zqGl3FPqr+GczGkw5m7kozWBB+mV5IGQEgRix810iNudegfBB2Keps5Xin/hOM4IIe4DPAQXj7GFLS0rmx2coAxwJBjyyY+dEtJgs2pTbDKMVCgU83DHQYN8/Koh8g15HsmSaiAmmj+2dHAdB04t89/1O/w1cDnyilFU=';
-$headers = [
-	'Authorization: Bearer ' . $channelToken,
-	'Content-Type: application/json; charset=utf-8',
-];
-
-// POSTデータを設定してJSONにエンコード
-$post = [
-	'to' => 'メモした Your userId の文字列',
-	'messages' => [
-		[
-			'type' => 'text',
-			'text' => 'hello world',
-		],
-	],
-];
-$post = json_encode($post);
-
-// HTTPリクエストを設定
-$ch = curl_init('https://api.line.me/v2/bot/message/push');
-$options = [
-	CURLOPT_CUSTOMREQUEST => 'POST',
-	CURLOPT_HTTPHEADER => $headers,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_BINARYTRANSFER => true,
-	CURLOPT_HEADER => true,
-	CURLOPT_POSTFIELDS => $post,
-];
-curl_setopt_array($ch, $options);
-
-// 実行
-$result = curl_exec($ch);
-
-// エラーチェック
-$errno = curl_errno($ch);
-if ($errno) {
-	return;
+ 
+$accessToken = 'I4DUva3zqGl3FPqr+GczGkw5m7kozWBB+mV5IGQEgRix810iNudegfBB2Keps5Xin/hOM4IIe4DPAQXj7GFLS0rmx2coAxwJBjyyY+dEtJgs2pTbDKMVCgU83DHQYN8/Koh8g15HsmSaiAmmj+2dHAdB04t89/1O/w1cDnyilFU=';
+ 
+//ユーザーからのメッセージ取得
+$json_string = file_get_contents('php://input');
+$json_object = json_decode($json_string);
+ 
+//取得データ
+$replyToken = $json_object->{"events"}[0]->{"replyToken"};        //返信用トークン
+$message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッセージタイプ
+$message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
+ 
+//メッセージタイプが「text」以外のときは何も返さず終了
+if($message_type != "text") exit;
+ 
+//返信メッセージ
+$return_message_text = "「" . $message_text . "」じゃねーよｗｗｗ";
+ 
+//返信実行
+sending_messages($accessToken, $replyToken, $message_type, $return_message_text);
+?>
+<?php
+//メッセージの送信
+function sending_messages($accessToken, $replyToken, $message_type, $return_message_text){
+    //レスポンスフォーマット
+    $response_format_text = [
+        "type" => $message_type,
+        "text" => $return_message_text
+    ];
+ 
+    //ポストデータ
+    $post_data = [
+        "replyToken" => $replyToken,
+        "messages" => [$response_format_text]
+    ];
+ 
+    //curl実行
+    $ch = curl_init("https://api.line.me/v2/bot/message/reply");
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json; charser=UTF-8',
+        'Authorization: Bearer ' . $accessToken
+    ));
+    $result = curl_exec($ch);
+    curl_close($ch);
 }
-
-// HTTPステータスを取得
-$info = curl_getinfo($ch);
-$httpStatus = $info['http_code'];
-
-$responseHeaderSize = $info['header_size'];
-$body = substr($result, $responseHeaderSize);
-
-// 200 だったら OK
-echo $httpStatus . ' ' . $body;
+?>
