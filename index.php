d8c890f9ac8b83e62557805934294a9988572520
廣田真由 <my-hrt@i.softbank.jp>
2018-07-11 21:49:15 +0900

    チャップリン

--- index.php
+++ index.php
@@ -7,19 +7,20 @@ $json_string = file_get_contents('php://input');
 $json_object = json_decode($json_string);
  
 //取得データ
-$replyToken = $json_object->{"events"}[0]->{"replyToken"};        //返信用トークン
-$message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッセージタイプ
-$message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
+$replyToken   = $json_object->{"events"}[0]->{"replyToken"};         //返信用トークン
+$message_type = $json_object->{"events"}[0]->{"message"}->{"type"};  //メッセージタイプ
+$message_text = $json_object->{"events"}[0]->{"message"}->{"text"};  //メッセージ内容
  
 //メッセージタイプが「text」以外のときは何も返さず終了
 if($message_type != "text") exit;
  
 //返信メッセージ
-$return_message_text = "「" . $message_text . "」じゃねーよｗｗｗ";
+$return_message_text = "下を向いていたら、虹を見つけることは出来ないよ。";
  
 //返信実行
 sending_messages($accessToken, $replyToken, $message_type, $return_message_text);
 ?>
+
 <?php
 //メッセージの送信
 function sending_messages($accessToken, $replyToken, $message_type, $return_message_text){
