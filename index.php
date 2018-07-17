f89219df6b6b2c52d04c1911ccc0d1557b1ed665
廣田真由 <my-hrt@i.softbank.jp>
2018-07-17 20:52:27 +0900

    本2

--- index.php
+++ index.php
@@ -2,8 +2,21 @@
 // sdkを読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 
-// POSTメソッドで渡される値を取得、表示
-$inputString = file_get_contents('php://input');
-error_log($inputString);
+//アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('LINE_CHANNEL_TOKEN'));
 
+// CulHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient,['channelSecret' => getnev('LINE_CHANNEL_SECRET')]);
+
+// LINE Messaging APIがリクエストに付与した署名を取得
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+// 署名が正当かチェック。正当であればリクエストをパースし配列へ
+$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
+
+// 配列に格納された各イベントをループで処理
+foreach ($events as $event) {
+    // テキストを返信
+    $bot->replyText($event->getReplyToken(),'TextMessage');
+}
 ?>
